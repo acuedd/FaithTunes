0221b8f053f2b0dfd1ffb110eed188e94f591cbb
acuedd <acued89@gmail.com>
2025-05-28 14:21:51 -0600

    fix: fixing order to function to send right the values

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -8,7 +8,7 @@ export const loginUser = async (email: string, password: string) => {
   return response.data;
 };
 
-export const registerUser = async (email: string, password: string, name: string) => {
+export const registerUser = async (name: string, email: string, password: string) => {
   const response = await axios.post(`${API}/auth/register`, {
     email,
     password,
